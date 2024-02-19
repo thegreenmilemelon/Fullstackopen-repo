79bdb04e99b5f43b602d7f28118b4f3fd261d9c0
greenmelon <suren78621@gmail.com>
2024-02-19 23:52:09 +0545

    exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 
 const StatisticLine = (props) => {
   return (
-    <div>
-      <p>
-        {props.text} {props.value}
-      </p>
-    </div>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   );
 };
 
@@ -20,23 +19,30 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine
-        text="all"
-        value={props.good + props.neutral + props.bad}
-      />
-      <StatisticLine
-        text="average"
-        value={
-          (props.good - props.bad) / (props.good + props.neutral + props.bad)
-        }
-      />
-      <StatisticLine
-        text="positive"
-        value={(props.good / (props.good + props.neutral + props.bad)) * 100}
-      />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine
+            text="all"
+            value={props.good + props.neutral + props.bad}
+          />
+          <StatisticLine
+            text="average"
+            value={
+              (props.good - props.bad) /
+              (props.good + props.neutral + props.bad)
+            }
+          />
+          <StatisticLine
+            text="positive"
+            value={
+              (props.good / (props.good + props.neutral + props.bad)) * 100
+            }
+          />
+        </tbody>
+      </table>
     </div>
   );
 };
