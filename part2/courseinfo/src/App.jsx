e63cc0dfa9c17723c74fcfbdb608cbf8c570c049
greenmelon <suren78621@gmail.com>
2024-02-20 16:41:19 +0545

    exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const Header = ({ head }) => {
+  return (
+    <div>
+      <h1>{head}</h1>
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <div>Hello</div>
+      <Header head={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
