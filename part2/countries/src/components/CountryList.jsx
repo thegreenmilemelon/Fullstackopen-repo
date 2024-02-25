5b9c9a20f1736df134029f1acbf04ed4f02ca3aa
greenmelon <suren78621@gmail.com>
2024-02-25 22:12:04 +0545

    exercise 2.20

--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -1,8 +1,69 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function CountryList({ countries, filterCountry }) {
   console.log(countries);
   const [isDisplay, setIsDisplay] = useState(false);
+  const [weather, setWeather] = useState();
+
+  const api_key = import.meta.env.VITE_API_KEY;
+  const iconURL = "https://openweathermap.org/img/wn/";
+
+  useEffect(() => {
+    if (countries.length === 1) {
+      const capital = countries[0].capital[0];
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`
+        )
+        .then((response) => {
+          console.log("weather data", response.data);
+          setWeather(response.data);
+        });
+    }
+  }, [countries]);
+
+  const getWeatherIcon = (weatherCode) => {
+    switch (Math.floor(weatherCode / 100)) {
+      case 2:
+        return "11d.png";
+      case 3:
+        return "09d.png";
+      case 5:
+        switch (weatherCode) {
+          case 500:
+          case 501:
+          case 502:
+          case 503:
+          case 504:
+            return "10d.png";
+          case 511:
+            return "13d.png";
+          default:
+            return "09d.png";
+        }
+      case 6:
+        return "13d.png";
+      case 7:
+        return "50d.png";
+      case 8:
+        switch (weatherCode) {
+          case 800:
+            return "01d.png";
+          case 801:
+            return "02d.png";
+          case 802:
+            return "03d.png";
+          case 803:
+          case 804:
+            return "04d.png";
+          default:
+            return "01d.png";
+        }
+      default:
+        return "01d.png";
+    }
+  };
 
   const toggleDisplay = (countryName) => {
     setIsDisplay(countryName.ccn3 === isDisplay ? false : countryName.ccn3);
@@ -28,6 +89,24 @@ function CountryList({ countries, filterCountry }) {
           alt={`Flag of ${country.name.common}`}
           style={{ width: "100px" }}
         />
+        <h3>Weather in {country.capital}</h3>
+        {weather && (
+          <div>
+            <p>
+              <strong>Temperature:</strong> {weather.main.temp} Celsius
+            </p>
+            <img
+              src={`${iconURL}${getWeatherIcon(weather.weather[0].id)}`}
+              alt=""
+            />
+            <p>
+              <strong>Wind:</strong> {weather.wind.speed} m/s
+            </p>
+            <p>
+              <strong>Description:</strong> {weather.weather[0].description}
+            </p>
+          </div>
+        )}
       </div>
     );
   };
