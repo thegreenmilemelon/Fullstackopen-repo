565a8f538a423d276a9990eb8b2201beae219419
greenmelon <suren78621@gmail.com>
2024-03-09 05:46:06 +0545

    exercise 4.3-4.7

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -1,7 +1,53 @@
+const _ = require("lodash");
+
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes;
+  };
+  return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const reducer = (max, blog) => {
+    return max.likes > blog.likes ? max : blog;
+  };
+
+  const result = blogs.reduce(reducer, blogs[0]);
+  return {
+    title: result.title,
+    author: result.author,
+    likes: result.likes,
+  };
+};
+
+const mostBlogs = (blogs) => {
+  const authors = _.countBy(blogs, "author");
+  const mostAuthor = _.maxBy(_.keys(authors), (key) => authors[key]);
+  return {
+    author: mostAuthor,
+    blogs: authors[mostAuthor],
+  };
+};
+
+const mostLikes = (blogs) => {
+  const authors = _.groupBy(blogs, "author");
+  const likes = _.mapValues(authors, (blog) => _.sumBy(blog, "likes"));
+  const mostLikes = _.maxBy(_.keys(likes), (key) => likes[key]);
+  return {
+    author: mostLikes,
+    likes: likes[mostLikes],
+  };
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
