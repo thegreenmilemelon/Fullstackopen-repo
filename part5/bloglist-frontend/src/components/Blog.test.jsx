0c1c5c3aef37f7140ed0c27dbcfaafa45b5d1a58
greenmelon <suren78621@gmail.com>
2024-04-01 15:57:28 +0545

    exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import Blog from "./Blog";
-import { expect } from "vitest";
+import { expect, test } from "vitest";
 
 test("renders blog title and author, but not URL and likes by default", async () => {
   const blog = {
@@ -30,4 +30,33 @@ test("renders blog title and author, but not URL and likes by default", async ()
   expect(likesElement).toBeNull();
 
   expect(element).toBeDefined();
+});
+
+//Make a test, which checks that the blog's URL and number of likes are shown when the button controlling the shown details has been clicked.
+
+test("the url and number of likes are shown when the button controlling the shown details has been clicked", async () => {
+  const blog = {
+    title: "Test Blog",
+    author: "John Doe",
+    url: "example.com",
+    likes: 10,
+    user: {
+      username: "testuser",
+      name: "Test User",
+    },
+  };
+
+  const changeLike = vi.fn();
+  const removeBlog = vi.fn();
+
+  render(<Blog blog={blog} changeLike={changeLike} removeBlog={removeBlog} />);
+
+  const user = userEvent.setup();
+
+  const button = screen.getByText("view");
+  await user.click(button);
+  const urlElement = screen.getByText("example.com");
+  expect(urlElement).toBeDefined();
+  const likesElement = screen.getByText("likes 10");
+  expect(likesElement).toBeDefined();
 });
