b0b813c9aa8d9d9533c54b91f0a8d03dfd90374e
greenmelon <suren78621@gmail.com>
2024-04-05 10:46:28 +0545

    exercise 6.5

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -17,10 +17,12 @@ const App = () => {
     dispatch(createAnecdote(content));
   };
 
+  const ordered = [...anecdotes].sort((a, b) => b.votes - a.votes);
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
+      {ordered.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
