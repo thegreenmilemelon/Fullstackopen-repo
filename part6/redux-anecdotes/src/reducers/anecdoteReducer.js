34af4463d1f2d30292482864cb301269e62eaba4
greenmelon <suren78621@gmail.com>
2024-04-07 00:31:24 +0545

    exercise 6.16-6.17

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,16 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
   initialState: [],
   reducers: {
-    createAnecdote(state, action) {
-      state.push(action.payload);
-    },
     votes(state, action) {
       const id = action.payload;
       const index = state.findIndex((anecdote) => anecdote.id === id);
       state[index].votes += 1;
+    },
+    appendAnecdote(state, action) {
+      state.push(action.payload);
     },
     setAnecdotes(state, action) {
       return action.payload;
@@ -18,5 +19,20 @@ const anecdoteSlice = createSlice({
   },
 });
 
-export const { createAnecdote, votes, setAnecdotes } = anecdoteSlice.actions;
+export const { votes, setAnecdotes, appendAnecdote } = anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch(appendAnecdote(newAnecdote));
+  };
+};
+
 export default anecdoteSlice.reducer;
