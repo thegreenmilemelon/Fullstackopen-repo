92c008cbf19acd929409ba6d323b377f241fe753
greenmelon <suren78621@gmail.com>
2024-04-17 23:11:47 +0545

    exercise 8.6

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -118,6 +119,15 @@ const typeDefs = `
     allBooks(author: String, genre: String) : [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]
+    ): Book
+  }
 `;
 
 const resolvers = {
@@ -150,6 +160,20 @@ const resolvers = {
       }
     },
     allAuthors: () => authors,
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      if (!authors.find((a) => a.name === args.author)) {
+        authors = authors.concat({
+          name: args.author,
+          id: uuid(),
+        });
+      }
+      return book;
+    },
   },
 };
 
