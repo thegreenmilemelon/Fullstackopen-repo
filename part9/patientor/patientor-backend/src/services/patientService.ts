5815447704bd2a64d16a7140d6ffc58298b0df30
greenmelon <suren78621@gmail.com>
2024-05-12 16:13:26 +0545

    exercise 9.11

---
+++ part9/patientor/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,21 @@
+import patients from "../../data/patients";
+import { Patient, NonSensitivePatientEntry } from "../types";
+
+const getEntries = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default {
+  getEntries,
+  getNonSensitiveEntries,
+};
--- part9/patientor/patientor-backend/src/types.ts
+++ part9/patientor/patientor-backend/src/types.ts
@@ -1,5 +1,18 @@
+export type Gender = "male" | "female" | "other";
+
 export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
+
+export interface Patient {
+  id: string;
+  name: string;
+  occupation: string;
+  gender: Gender;
+  ssn?: string;
+  dateOfBirth?: string;
+}
+
+export type NonSensitivePatientEntry = Omit<Patient, "ssn">;
