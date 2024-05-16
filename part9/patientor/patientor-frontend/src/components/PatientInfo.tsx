2154b3e19b5c1ac17436350d0aed10678265c7fd
greenmelon <suren78621@gmail.com>
2024-05-16 16:23:03 +0545

    exercise 9.24

--- part9/patientor/patientor-frontend/src/components/PatientInfo.tsx
+++ part9/patientor/patientor-frontend/src/components/PatientInfo.tsx
@@ -1,20 +1,26 @@
 import { Typography, Paper, Grid } from "@mui/material";
-import { Patient, Gender } from "../types";
+import { Patient, Gender, Diagnosis } from "../types";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import patientService from "../services/patients";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
+import diagnosisService from "../services/diagnosis";
 
 const PatientInfo = () => {
   const { id } = useParams();
   const [selectedPatient, setSelectedPatient] = useState<Patient | null>(null);
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
 
   useEffect(() => {
     const fetchPatient = async () => {
       if (id) {
         const patient = await patientService.getPatient(id);
         setSelectedPatient(patient);
+
+        const diagnoses = await diagnosisService.getAll();
+        setDiagnoses(diagnoses);
+        console.log("diagnoses", diagnoses);
       }
     };
 
@@ -70,7 +76,9 @@ const PatientInfo = () => {
               </Typography>
               <ul>
                 {entry.diagnosisCodes?.map((code, index) => (
-                  <li key={`${entry.id}-${index}`}>{code}</li>
+                  <li key={`${entry.id}-${index}`}>
+                    {code} {diagnoses.find((d) => d.code === code)?.name}
+                  </li>
                 ))}
               </ul>
             </div>
